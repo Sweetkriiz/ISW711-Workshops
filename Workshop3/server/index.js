4348f2160095e05e83fb7eaef7c7457d4f8500e4
Krisley Castro <kriizcastro@hotmail.com>
2026-02-16 20:45:30 -0600

    Backend: configuración principal del servidor Express y MongoDB

---
+++ Workshop3/server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const courseRoutes = require('./routes/routes');
+
+const app = express(); 
+
+// Middlewares
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: '*',
+  methods: '*'
+}));
+
+// Rutas
+app.use('/api', courseRoutes);
+
+// Conexión a MongoDB
+mongoose.connect(process.env.DATABASE_URL);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
+
+// Start server
+app.listen(3001, () =>
+  console.log(`UTN API service listening on port 3001!`)
+);
